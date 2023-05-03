ab5f3e524723eb2f5fd1ed76057d6ff12be3bec2
Alexandre Ramos <alexandreramos@MacBook-Pro-de-Alexandre.local>
2023-05-03 19:04:29 -0300

    toLowerCase helper function removed

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,4 @@
 const toNormalNumber = (num: string, decimals: number) =>
   (parseFloat(num) / Math.pow(10, decimals)).toFixed(2).toString();
 
-const toLowerCase = (address: string) => address.toLowerCase();
-
 export { toNormalNumber };
